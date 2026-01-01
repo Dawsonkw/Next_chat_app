65484a18de4028951a6f47c80f28bf82a587cb83
Dawsonkw <dawsonkw@gmail.com>
2026-01-01 14:02:24 -0700

    building out file structure and components. Nextjs is wild.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase";
+import { Message } from "@/lib/types";
+
 export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const { data, error } = await supabase.from("messages").select("*");
+
+      if (error) console.error("Error:", error);
+      else setMessages(data || []);
+    }
+
+    fetchMessages();
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col items-center justify-center px-4 text-center">
-        <h1 className="text-5xl font-extrabold text-zinc-900 dark:text-white sm:text-6xl">
-          Chatbox.ts
-        </h1>
-        <p className="mt-6 text-lg text-zinc-700 dark:text-zinc-300 sm:text-xl">
-          an open source peer to peer chat application built with Next.js,
-          TypeScript, Tailwind CSS, and Supabase.
-        </p>
-      </main>
+    <div>
+      <h1>Messages: {messages.length}</h1>
     </div>
   );
 }
---
+++ lib/types.ts
@@ -0,0 +1,8 @@
+type Message = {
+  id: number;
+  created_at: string;
+  content: string;
+  user_name: string;
+};
+
+export type { Message };
